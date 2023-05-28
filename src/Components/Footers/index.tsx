edeb542dea4e2bcc531e33710fd29b5bbc87f619
joaquin <joacodi2001@gmail.com>
2023-05-28 18:43:25 -0300

    Add Form

--- src/Components/Footers/index.tsx
+++ src/Components/Footers/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Form, Input, Checkbox, Button } from 'antd';
+import { Form, Input, Button } from 'antd';
 
 import styles from '../../styles/Footer.module.css';
 
