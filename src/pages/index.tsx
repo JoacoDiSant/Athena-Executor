64959f29d2e6c33bdc389c8a027be039cd3a56f9
Joaquin Di Santo <joacodi2001@gmail.com>
2024-07-16 16:52:12 -0300

    Fix some Scripts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable max-len */
 /* eslint-disable react/jsx-sort-props */
 import Head from 'next/head';
-import Script from 'next/script';
 
 import App from '@/Components';
 
@@ -9,6 +8,19 @@ export default function Home() {
   return (
     <>
       <Head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-10808154807"
+        />
+        <script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'AW-10808154807');
+        `}
+        </script>
         <title>
           Cerrajería 24 Horas - Mar del Plata - Rey Mendez Seguridad
         </title>
@@ -26,19 +38,6 @@ export default function Home() {
         <meta name="og:title" content="Reparación de cerraduras a domicilio" />
         <meta content="index" name="robots" />
         <script src="/static/script.js" type="text/javascript" />
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=AW-10808154807"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'AW-10808154807');
-        `}
-        </Script>
       </Head>
       <main>
         <App />
